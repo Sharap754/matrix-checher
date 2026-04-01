eb5f89c08da1b72a6b571fe89d64c73b4cb014a7
Sharap754 <lyakkoev.pavel04@gmail.com>
2026-04-01 21:33:33 +0300

    last

--- cicd/test.sh
+++ cicd/test.sh
@@ -1,2 +1,8 @@
-gcc test.cpp -o tests
-./tests
+output=$(printf "2\n1 2\n3 4\n1 3\n2 4\n" | usr/bin/matrix_check)
+
+if [[ "$output" == *"YES"* ]]; then
+    echo "Test 1 OK"
+else
+    echo "Test 1 FAIL"
+    exit 1
+fi
