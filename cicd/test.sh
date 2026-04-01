5184890a91715fdcbb3a73fef8303f9c56c0b931
Sharap754 <lyakkoev.pavel04@gmail.com>
2026-04-01 21:40:00 +0300

    last

--- cicd/test.sh
+++ cicd/test.sh
@@ -1,4 +1,4 @@
-output=$(printf "2\n1 2\n3 4\n1 3\n2 4\n" | usr/bin/matrix_check)
+output=$(printf "2\n1 2\n3 4\n1 3\n2 4\n" | ../usr/bin/matrix_check)
 
 if [[ "$output" == *"YES"* ]]; then
     echo "Test 1 OK"
