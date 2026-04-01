07d232fdcc06a7e550b185ab5d34336e20bea02d
Sharap754 <lyakkoev.pavel04@gmail.com>
2026-04-01 21:28:39 +0300

    last

--- cicd/test.cpp
+++ cicd/test.cpp
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 int main() {
-    int result = system("printf \"2\n1 2\n3 4\n1 3\n2 4" | ../usr/bin/matrix_check");
+    int result = system("printf \"2\\n1 2\\n3 4\\n1 3\\n2 4\\n\" | ../usr/bin/matrix_check");
     if (result == "YES") {
         printf("Test passed\n");
         return 1;
